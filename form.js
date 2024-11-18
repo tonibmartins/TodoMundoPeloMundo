ee8ba77f58b48ac88a8bcb3f2c41bb2178127dbc
Antônio Augusto Martins <66620565+tonibmartins@users.noreply.github.com>
2024-11-18 09:52:46 -0300

    Create form.js

---
+++ form.js
@@ -0,0 +1,23 @@
+(function() {
+    emailjs.init("1BT1iKz7d09IJLK88"); // Substitua pelo seu user ID do EmailJS
+})();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const today = new Date().toISOString().split('T')[0];
+    document.getElementById('data').setAttribute('min', today);
+});
+
+function sendEmail(event) {
+    event.preventDefault();
+
+    const form = event.target;
+
+    emailjs.sendForm('register_tnpm', 'template_5gme6zn', form)
+        .then(() => {
+            alert('Formulário enviado com sucesso!');
+            form.reset();
+        }, (error) => {
+            console.error('Erro ao enviar formulário:', error);
+            alert('Ocorreu um erro ao enviar o formulário. Tente novamente mais tarde.');
+        });
+}
